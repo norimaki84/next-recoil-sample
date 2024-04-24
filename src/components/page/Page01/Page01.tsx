5d09f3294fb0fe5a718749136e9e62f199f2b2b7
Norikazu Teraguchi <norimaki.tera@gmail.com>
2024-04-24 17:55:47 +0900

    :+1: modify

--- src/components/page/Page01/Page01.tsx
+++ src/components/page/Page01/Page01.tsx
@@ -3,9 +3,20 @@ import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Meta } from '~/components/Meta'
 
+import { useRecoilValue } from 'recoil'
+import { testValState } from '~/store/atoms/commonAtoms'
+
 import styles from './Page01.module.scss'
 
 export const Page01: NextPage = () => {
+  const text = useRecoilValue(testValState)
+
+  useEffect(() => {
+    if (text === '') {
+      // ページリダイレクト処理
+    }
+  }, [])
+
   return (
     <div className={styles['container']}>
       <Meta id="page01" />
@@ -19,6 +30,12 @@ export const Page01: NextPage = () => {
           <Link href={'/page02'}>Page02</Link>
         </li>
       </ul>
+
+      {/* */}
+      <div>
+        <p>You entered: {text}</p>
+      </div>
+      {/*  */}
     </div>
   )
 }
--- src/components/page/Top/Top.tsx
+++ src/components/page/Top/Top.tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from 'next'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, ChangeEvent } from 'react'
 import Link from 'next/link'
+
+
 import { Meta } from '~/components/Meta'
+
+import { useRecoilState } from 'recoil'
+
+import { testValState } from '~/store/atoms/commonAtoms'
 
 import styles from './Top.module.scss'
 
 export const Top: NextPage = () => {
+  const [text, setText] = useRecoilState(testValState)
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setText(e.target.value)
+  }
+
   return (
     <div className={styles['container']}>
       <Meta id="top" />
@@ -13,12 +25,18 @@ export const Top: NextPage = () => {
       <ul>
         <li>
           <Link href={'/page01'}>Page01</Link>
+          {/*<a href={'/page01'}>Page01：ただのaタグ</a>*/}
         </li>
-
         <li>
           <Link href={'/page02'}>Page02</Link>
         </li>
       </ul>
+      {/*　*/}
+      <div>
+        <input type="text" value={text} onChange={handleChange} />
+        <p>You entered: {text}</p>
+      </div>
+      {/*  */}
     </div>
   )
 }
